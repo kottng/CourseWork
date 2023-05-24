da624cb99dff98ff27255e6a32883ac2f18d2da9
Dmitriy Pavlovich <75154790+kottng@users.noreply.github.com>
2023-05-24 10:26:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-
-from PetriArc import PetriArc
-from PetriMapping import PetriMapping
-from PetriNet import PetriNet
-from PetriNode import PetriNode
+from myRelation.PetriMapping import PetriMapping
+from myRelation.PetriNet import PetriNet
+from myRelation.checkRelation import check_is_relation_an_alpha_morphism
 
 
-
-
-
-
-
-
-
-
-
-def check_is_transitions_correct(net):
-    list_of_transitions = net.transitions
-    for i in list_of_transitions.values():
-        if len(i.get_in_arcs()) != len(i.get_out_arcs()):
-            return False
-    return True
-
-# Задание 2 СП
+# Задание 2 Сетей Петри
 # Создаем объект сети Петри
 net1 = PetriNet()
 
@@ -34,14 +16,25 @@ net1.add_place("P3")
 net1.add_transition("T3")
 net1.add_transition("T4")
 net1.add_transition("T5")
+net1.add_transition("T6")
+net1.add_place("P4")
+net1.add_place("P5")
+net1.add_place("P6")
 
 # Добавляем дуги
+
+net1.add_arc("P1", "T1")
+net1.add_arc("P1", "T2")
 net1.add_arc("T1", "P2")
-net1.add_arc("T2", "P2")
+net1.add_arc("T2", "P3")
 net1.add_arc("P2", "T3")
-net1.add_arc("T3", "P3")
 net1.add_arc("P3", "T4")
-net1.add_arc("P3", "T5")
+net1.add_arc("T3", "P4")
+net1.add_arc("T4", "P5")
+net1.add_arc("P4", "T5")
+net1.add_arc("P5", "T6")
+net1.add_arc("T5", "P6")
+net1.add_arc("T6", "P6")
 
 # Получаем вершину P1
 p1 = net1.places["P1"]
@@ -61,16 +54,12 @@ net2.add_transition("R1")
 net2.add_place("Q2")
 net2.add_transition("R2")
 net2.add_place("Q3")
-net2.add_transition("R3")
-net2.add_transition("R4")
-
 # Добавляем дуги
 
+net2.add_arc("Q1", "R1")
 net2.add_arc("R1", "Q2")
-net2.add_arc("R2", "Q2")
-net2.add_arc("Q2", "R3")
-net2.add_arc("Q2", "R4")
-
+net2.add_arc("Q2", "R2")
+net2.add_arc("R2", "Q3")
 
 # Получаем вершину P1
 q1 = net2.places["Q1"]
@@ -98,11 +87,17 @@ for i in net2.transitions.values():
 for i in net2.places.values():
     list_of_places2.append(i)
 
+mapping.add_mapping(list_of_places1[0], list_of_places2[0])
+mapping.add_mapping(list_of_places1[5], list_of_places2[2])
+mapping.add_mapping(list_of_transitions1[0], list_of_transitions2[0])
+mapping.add_mapping(list_of_transitions1[1], list_of_transitions2[0])
 mapping.add_mapping(list_of_places1[1], list_of_places2[1])
 mapping.add_mapping(list_of_places1[2], list_of_places2[1])
 mapping.add_mapping(list_of_transitions1[2], list_of_places2[1])
-mapping.add_mapping(list_of_transitions1[0], list_of_transitions2[0])
-mapping.add_mapping(list_of_transitions1[1], list_of_transitions2[1])
-mapping.add_mapping(list_of_transitions1[3], list_of_transitions2[2])
-mapping.add_mapping(list_of_transitions1[4], list_of_transitions2[3])
-# используйте функции классов и функцию check_is_transitions_correct для анализа структурных отношений между моделями процессов, заданных в виде Сетей Петри
+mapping.add_mapping(list_of_transitions1[3], list_of_places2[1])
+mapping.add_mapping(list_of_places1[3], list_of_places2[1])
+mapping.add_mapping(list_of_places1[4], list_of_places2[1])
+mapping.add_mapping(list_of_transitions1[4], list_of_transitions2[1])
+mapping.add_mapping(list_of_transitions1[5], list_of_transitions2[1])
+# mapping.add_mapping(list_of_places1[2], list_of_places2[0])
+check_is_relation_an_alpha_morphism(net1, net2, mapping)
